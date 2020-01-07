c7155b2b5d4c86e1ebee40fb825429214f157235
Mridul Parakh <mridulparakh41@gmail.com>
2020-01-07 15:18:33 +0530

    first

--- router/nodemailer.js
+++ router/nodemailer.js
@@ -5,25 +5,25 @@ module.exports=function node(data){
 let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: process.env.EMAIL || 'jainmannu21@gmail.com', // TODO: your gmail account
-        pass: process.env.PASSWORD || 'rekha202' // TODO: your gmail password
+        user: process.env.EMAIL || 'jainmannu21@gmail.com', 
+        pass: process.env.PASSWORD || 'rekha202' 
     }
 });
 
-// Step 2
+
 var maillist=[
     "mridulparakh41@gmail.com",
     "katrinajain58@gmail.com"
 ]
 
 let mailOptions = {
-    from: 'jainmannu21@gmail.com', // TODO: email sender
-    to: maillist, // TODO: email receiver
+    from: 'jainmannu21@gmail.com', 
+    to: maillist, 
     subject: 'Nodemailer - Test',
     text: "Name:"+data.name+"\n Email: "+data.email+"\nPhone: "+data.phone+"\nCountry: "+data.country+"\nRegion: "+data.region
 };
 
-// Step 3
+
 transporter.sendMail(mailOptions, (err, data) => {
     if (err) {
         return console.log('Error occurs');
